9ffae4ee68be4f848496a7b74af3b561d79cc5ef
Jacek Lewiński <lewinski89@wp.pl>
2017-12-11 20:50:47 +0100

    First commit with simply index page

---
+++ src/main/java/pl/jaceklewinski/uberstat/UberstatApplication.java
@@ -0,0 +1,12 @@
+package pl.jaceklewinski.uberstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberstatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberstatApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/jaceklewinski/uberstat/UberstatApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.jaceklewinski.uberstat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UberstatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
