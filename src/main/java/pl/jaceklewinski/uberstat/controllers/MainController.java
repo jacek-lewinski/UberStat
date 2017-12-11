6058a2aeb80ee453364b10eddd8546381cf7d5c3
Jacek Lewiński <lewinski89@wp.pl>
2017-12-11 21:19:56 +0100

    Added link between form model class, index page and controller

--- src/main/java/pl/jaceklewinski/uberstat/controllers/MainController.java
+++ src/main/java/pl/jaceklewinski/uberstat/controllers/MainController.java
@@ -1,13 +1,16 @@
 package pl.jaceklewinski.uberstat.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.jaceklewinski.uberstat.models.forms.TripForm;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("tripForm", new TripForm());
 
         return "index";
     }
