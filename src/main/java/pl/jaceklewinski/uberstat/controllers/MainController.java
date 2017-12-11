b94d2a3bb29ef3bcd3bca233ea942fe09a159c97
Jacek Lewiński <lewinski89@wp.pl>
2017-12-11 21:32:42 +0100

    Added post mapping on index with simple debugging by sout whether form was send correctly

--- src/main/java/pl/jaceklewinski/uberstat/controllers/MainController.java
+++ src/main/java/pl/jaceklewinski/uberstat/controllers/MainController.java
@@ -3,6 +3,8 @@ package pl.jaceklewinski.uberstat.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.jaceklewinski.uberstat.models.forms.TripForm;
 
 @Controller
@@ -11,6 +13,13 @@ public class MainController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("tripForm", new TripForm());
+
+        return "index";
+    }
+
+    @PostMapping("/")
+    public String indexPost(@ModelAttribute("tripForm") TripForm tripForm) {
+        System.out.println(tripForm.getPrice());
 
         return "index";
     }
